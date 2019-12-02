c018dd6d25b6de145a93315aeedf220b9b612c74
keblazer <keblazer@student.codam.nl>
2019-12-02 15:48:24 +0100

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: keblazer <keblazer@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/11 17:12:28 by keblazer       #+#    #+#                */
-/*   Updated: 2019/12/02 12:48:59 by keblazer      ########   odam.nl         */
+/*   Updated: 2019/12/02 15:42:22 by keblazer      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	s = 0;
-	f = 0;
-	return (0);
+	unsigned int	size;
+	unsigned int	i;
+	char			*newstr;
+
+	size = ft_strlen(s);
+	if (!(newstr = (char*)malloc(sizeof(char) * size + 1)))
+	{
+		return (NULL);
+	}
+	i = -1;
+	while (++i < size)
+	{
+		if (s && f)
+		{
+			newstr[i] = f(i, s[i]);
+		}
+	}
+	newstr[i] = '\0';
+	return (newstr);
 }
