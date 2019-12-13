beffb4530b1be34462cab9c32e482bcfbdf148e6
keblazer <keblazer@student.codam.nl>
2019-12-13 12:25:49 +0100

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,10 +6,10 @@
 /*   By: keblazer <keblazer@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/11 17:13:19 by keblazer       #+#    #+#                */
-/*   Updated: 2019/12/10 16:53:52 by keblazer      ########   odam.nl         */
+/*   Updated: 2019/12/13 11:49:52 by keblazer      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n);
+char	*ft_itoa(int value, char *str, int base);
